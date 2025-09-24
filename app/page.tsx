364903d903c3b240de37a06d8347a08f74b08262
Azad Hüseyin Kılıç <azadhuseyinkilic@Azads-MacBook-Pro.local>
2025-09-24 16:25:51 +0300

    Assets and Metadataare included.

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@ const Home = () => {
   return (
     <div>
       <h1 className="h1-bold">Welcome to the world of Next.js 👋🏼</h1>
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to the world of Next.js 👋🏼
-      </h1>
     </div>
   );
 };
