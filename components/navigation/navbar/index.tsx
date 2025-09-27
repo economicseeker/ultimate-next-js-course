c5d35c10a287994de95574081b4e541e144fefdc
Azad Hüseyin Kılıç <azadhuseyinkilic@Azads-MacBook-Pro.local>
2025-09-27 14:08:07 +0300

    Mobile Nav and Nav Links are implemented.

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,10 +1,11 @@
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
+import MobileNavigation from "./MobileNavigation";
 import Theme from "./Theme";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light800_dark200 z-50 w-full gap-5 p-6  shadow-light-300 dark:shadow-none sm:px-12 fixed">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
@@ -19,6 +20,7 @@ const Navbar = () => {
       <p>Global Search</p>
       <div className="flex-between gap-5">
         <Theme />
+        <MobileNavigation />
       </div>
     </nav>
   );
