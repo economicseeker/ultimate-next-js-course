c00d12fd504c6a2726892212585d1e69669b634c
Azad Hüseyin Kılıç <azadhuseyinkilic@Azads-MacBook-Pro.local>
2025-09-27 15:32:37 +0300

    Left sidebar is implemented.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,19 @@
+import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
 import { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-      {children}
-    </div>
+      <div className="flex">
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div>{children}</div>
+        </section>
+      </div>
+    </main>
   );
 };
 
