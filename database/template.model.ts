6c79e3da21d0cef8a9638ac6ec5d69e2053e97c1
Azad Hüseyin Kılıç <azadhuseyinkilic1@gmail.com>
2025-09-30 19:49:31 +0300

    Implement Interaction Model

--- database/template.model.ts
+++
@@ -1,9 +0,0 @@
-import { model, models, Schema } from "mongoose";
-
-export interface IModel {}
-
-const ModelSchema = new Schema<IModel>();
-
-const Model = models?.model || model<IModel>("Model", ModelSchema);
-
-export default Model;
