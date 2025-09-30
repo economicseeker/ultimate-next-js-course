fdb039e949d19388adb7ff2634a4004f147728bd
Azad Hüseyin Kılıç <azadhuseyinkilic1@gmail.com>
2025-09-30 11:07:25 +0300

    Markdown Editor is implemeted.

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -5,6 +5,9 @@ import { useForm } from "react-hook-form";
 
 import { AskQuestionSchema } from "@/lib/validations";
 
+import { MDXEditorMethods } from "@mdxeditor/editor";
+import dynamic from "next/dynamic";
+import { useRef } from "react";
 import { Button } from "../ui/button";
 import {
   Form,
@@ -17,7 +20,13 @@ import {
 } from "../ui/form";
 import { Input } from "../ui/input";
 
+const Editor = dynamic(() => import("@/components/editor"), {
+  // Make sure we turn SSR off
+  ssr: false,
+});
+
 const QuestionForm = () => {
+  const editorRef = useRef<MDXEditorMethods>(null);
   const form = useForm({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
@@ -66,7 +75,13 @@ const QuestionForm = () => {
                 Detailed explanation of your problem{" "}
                 <span className="text-primary-500">*</span>
               </FormLabel>
-              <FormControl>Editor</FormControl>
+              <FormControl>
+                <Editor
+                  value={field.value}
+                  editorRef={editorRef}
+                  fieldChange={field.onChange}
+                />
+              </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Introduce the problem and expand on what you&apos;ve put in the
                 title.
