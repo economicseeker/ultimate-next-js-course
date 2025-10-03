35f3856459ceee0f11684db06ba99a80d3e0f24f
Azad Hüseyin Kılıç <azadhuseyinkilic1@gmail.com>
2025-10-03 17:08:45 +0300

    Create Question Form Logic is implemented.

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,6 +1,13 @@
+import { auth } from "@/auth";
 import QuestionForm from "@/components/forms/QuestionForm";
+import ROUTES from "@/constants/routes";
+import { redirect } from "next/navigation";
 
-const AskAQuestion = () => {
+const AskAQuestion = async () => {
+  const session = await auth();
+  if (!session) {
+    redirect(ROUTES.SIGN_IN);
+  }
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
