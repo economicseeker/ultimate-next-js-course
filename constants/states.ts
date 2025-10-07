c08cf65070819aef35ad0adb8b6370d66d1e944e
Azad Hüseyin Kılıç <azadhuseyinkilic1@gmail.com>
2025-10-07 14:28:39 +0300

    Display All Users funct. is implemented.

--- constants/states.ts
+++ constants/states.ts
@@ -53,3 +53,8 @@ export const EMPTY_ANSWERS = {
   message:
     "The answer board is empty. Make it rain with your brilliant answer.",
 };
+
+export const EMPTY_USERS = {
+  title: "No Users Found",
+  message: "You are ALONE. The only one here. More users are on their way.",
+};
