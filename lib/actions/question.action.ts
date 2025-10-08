b58f462a73a0b0556c2af07fde3b798198279484
Azad Hüseyin Kılıç <azadhuseyinkilic1@gmail.com>
2025-10-08 15:01:15 +0300

    react cache for double fetching beacause of dynamic metadata is implemented.

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -20,6 +20,7 @@ import {
   PaginatedSearchParamsSchema,
 } from "@/lib/validations";
 
+import { cache } from "react";
 import dbConnect from "../mongoose";
 import { createInteraction } from "./interaction.action";
 
@@ -200,7 +201,7 @@ export async function editQuestion(
   }
 }
 
-export async function getQuestion(
+export const getQuestion = cache(async function getQuestion(
   params: GetQuestionParams
 ): Promise<ActionResponse<Question>> {
   const validationResult = await action({
@@ -225,7 +226,7 @@ export async function getQuestion(
   } catch (error) {
     return handleError(error) as ErrorResponse;
   }
-}
+});
 
 export async function getRecommendedQuestions({
   userId,
