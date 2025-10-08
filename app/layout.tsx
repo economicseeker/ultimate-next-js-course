7f3aabb88d261a0719420d3a57aa7d66e5da5919
Azad Hüseyin Kılıç <azadhuseyinkilic1@gmail.com>
2025-10-08 15:05:59 +0300

    Metada is updated.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "@/app/globals.css";
 import { auth } from "@/auth";
 import { Toaster } from "@/components/ui/sonner";
 import ThemeProvider from "@/context/Theme";
-import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import localFont from "next/font/local";
 import { ReactNode } from "react";
@@ -18,12 +17,75 @@ const spaceGrotesk = localFont({
   weight: "100 200 300 400 500 600 700 800 900",
 });
 
-export const metadata: Metadata = {
-  title: "DevFlow",
+export const metadata = {
+  title: "Dev Overflow",
   description:
-    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+    "Dev Overflow is a community-driven platform to ask and answer real-world programming questions. Learn, grow, and connect with developers around the world.",
+
+  generator: "Next.js",
+  applicationName: "Dev Overflow",
+  referrer: "origin-when-cross-origin",
+
+  keywords: [
+    "Dev Overflow",
+    "programming questions",
+    "developer Q&A",
+    "web development",
+    "JavaScript",
+    "React",
+    "Node.js",
+    "algorithms",
+    "data structures",
+    "developer community",
+  ],
+
+  authors: [
+    { name: "Azad Hüseyin Kılıç" },
+    { name: "Dev Overflow Team", url: "https://devoverflow.dev/team" },
+  ],
+  creator: "Adrian",
+  publisher: "Dev Overflow",
+
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+
   icons: {
-    icon: "/images/site-logo.svg",
+    icon: "/images/site-logo.svg", // regular favicon
+    shortcut: "/favicon.ico", // browser address bar icon
+    apple: "/apple-touch-icon.png", // Apple devices
+    other: [
+      {
+        rel: "mask-icon",
+        url: "/safari-pinned-tab.svg",
+        color: "#5bbad5",
+      },
+    ],
+  },
+
+  // Optional: Theme color for browser UI and mobile experience
+  themeColor: "#18181b",
+
+  // Optional: Color for Microsoft tiles and pinned sites
+  msapplication: {
+    TileColor: "#ffffff",
+    TileImage: "/mstile-150x150.png",
   },
 };
 
